c39739f74aaf47a3ff6bba8b35e8150ef0940612
N4choWasTaken <thierrygmuer@outlook.com>
2024-05-30 23:12:43 +0200

    :lipstick: introduced theme, changed structure of page

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,12 +7,16 @@ interface MainLayoutProps {
 
 export default function MainLayout(props: MainLayoutProps) {
   return (
-    <div className="main-layout">
-      <Header />
-      <div className="main-layout__content">
-        {props.children}
-      </div>
-      <Footer />
+    <div className="h-screen w-screen grid grid-rows-12">
+        <div className="row-span-1 bg-base">
+            <Header />
+        </div>
+        <div className="row-span-10">
+            {props.children}
+        </div>
+        <div className="row-span-1 bg-base">
+            <Footer />
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+      colors: {
+          "primary": "#F48024",
+          "secondary": "#7E3F94",
+          "white": "#FFFFFF",
+          "black": "#000000",
+          "base": "#D9D9D9", 
+        },
     extend: {},
   },
   plugins: [],
