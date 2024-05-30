ee5ebc8f1f925f4448146773779169f99f77b85b
N4choWasTaken <thierrygmuer@outlook.com>
2024-05-30 22:51:22 +0200

    :lipstick: installed tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-            <div className="header__content">
+            <div className="w-full h-full bg-slate-600">
                 <p>Header</p>
             </div>
         </div>
--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,5 +1,3 @@
-import MainLayout from '../layouts/MainLayout'
-
 export default function Cv() {
   return (
     <>
